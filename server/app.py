015c2aa93722e6495a7d2676346be8fa5f5fab08
Matteo Piccini <gnappo.1@live.it>
2024-11-20 15:47:17 -0800

    end of lab walkthrough

--- server/app.py
+++ server/app.py
@@ -14,9 +14,27 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    try:
+        if request.method == "GET":
+            #! how do I query all message OBJECTS
+            messages = Message.query
+            #! what can I return?
+            return make_response([message_obj.to_dict() for message_obj in messages], 200)
+        else:
+            #! 1. Retrieve data from the request body (JSON)
+            data = request.get_json()
+            #! 2. Instantiate a message object (use Message) and pass the info along
+            new_message = Message(body=data.get("body", "default text"), username=data.get("username"))
+            #! 3. Don't forget to add the new object to the session for tracking (and automatic SQL statement creation)
+            db.session.add(new_message)
+            #! 4. Commit the session to fire those SQL statements
+            db.session.commit()
+            #! return what you created!!!
+            return make_response(jsonify(new_message.to_dict()), 201)
+    except Exception as e:
+        return make_response({"error": str(e)}), 400
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
